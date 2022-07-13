2cc5fbd44b372ef866bead887bd5261b2ac8c6f0
Idan Dardikman <idandrd@gmail.com>
2022-07-13 18:24:10 +0300

    wip

--- dbx-ding/popup.js
+++ dbx-ding/popup.js
@@ -1,9 +1,19 @@
-document.getElementById('classicRadio').addEventListener('change', () => setSound('classic.mp3'))
-document.getElementById('scifiRadio').addEventListener('change', () => setSound('scifi.wav'))
-document.getElementById('gameRadio').addEventListener('change', () => setSound('game.wav'))
+document
+  .getElementById("classicRadio")
+  .addEventListener("change", () => setSound("classic.mp3"));
+document
+  .getElementById("scifiRadio")
+  .addEventListener("change", () => setSound("scifi.wav"));
+document
+  .getElementById("gameRadio")
+  .addEventListener("change", () => setSound("game.wav"));
+
+document.getElementById("soundSwitch").addEventListener("change", (event) => {
+  console.log(event.target.checked);
+});
 
 function setSound(value) {
-  chrome.storage.local.set({'sound': value}, function() {
-    console.log('Value is set to ' + value);
+  chrome.storage.local.set({ sound: value }, function () {
+    console.log("Value is set to " + value);
   });
 }
