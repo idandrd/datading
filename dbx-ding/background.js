dc288fa9dee4c61bb1e4ca9b2c3eea3e09fb9150
Idan Dardikman <idandrd@gmail.com>
2022-07-18 12:07:54 +0300

    send to tab works

--- dbx-ding/background.js
+++ dbx-ding/background.js
@@ -1,10 +1,27 @@
+const notications = {};
+
 chrome.runtime.onMessage.addListener(({ resultStats }, sender) => {
-  chrome.tabs.update(sender.tab.id, { active: true });
-  chrome.windows.update(sender.tab.windowId, {focused: true});
-  chrome.notifications.create(resultStats, {
-    type: "basic",
-    title: "Databricks Cell Run Complete",
-    message: resultStats,
-    iconUrl: "images/icons/38.png",
-  });
+  chrome.notifications.create(
+    resultStats,
+    {
+      type: "basic",
+      title: "Databricks Cell Run Complete",
+      message: resultStats,
+      iconUrl: "images/icons/38.png",
+    },
+    (notificationId) => {
+      notications[notificationId] = {
+        tabId: sender.tab.id,
+        windowId: sender.tab.windowId,
+      };
+    }
+  );
+});
+
+chrome.notifications.onClicked.addListener((notificationId) => {
+  if (notificationId in notications) {
+    const { tabId, windowId } = notications[notificationId];
+    chrome.tabs.update(tabId, { active: true });
+    chrome.windows.update(windowId, { focused: true });
+  }
 });
